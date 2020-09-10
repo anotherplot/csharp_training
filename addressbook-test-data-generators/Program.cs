80eb6098846ccf10f77879f46ad10ce670af06c5
olgamezhova <o.mezhova@dodopizza.com>
2020-09-10 20:57:14 +0300

    Add project info for test data generation

---
+++ addressbook-test-data-generators/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using NUnit.Framework;
+using WebAddressBookTests;
+
+namespace addressbook_test_data_generators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = Convert.ToInt32(args[0]);
+            StreamWriter writer = new StreamWriter(args[1]);
+            string format = args[2];
+
+            List<GroupData> groups = new List<GroupData>();
+
+            for (var i = 0; i < count; i++)
+            {
+                groups.Add(new GroupData(TestBase.GenerateRandomString(10))
+                {
+                    Header = TestBase.GenerateRandomString(100),
+                    Footer = TestBase.GenerateRandomString(100)
+                });
+            }
+
+            if (format == "csv")
+            {
+                WriteGroupsToCsvFile(groups, writer);
+            }
+            else if (format == "xml")
+            {
+                WriteGroupsToXmlFile(groups, writer);
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized format " + format);
+            }
+
+            writer.Close();
+        }
+
+        static void WriteGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
+        {
+            foreach (var group in groups)
+            {
+                writer.WriteLine("${0},${1},${2}",
+                    group.Name,
+                    group.Header,
+                    group.Footer);
+            }
+        }
+
+        static void WriteGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<GroupData>)).Serialize(writer,groups);
+        }
+    }
+}
---
+++ addressook-test-data-generators/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace addressook_test_data_generators
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            // TestBase.GenerateRandomString();
+        }
+    }
+}
