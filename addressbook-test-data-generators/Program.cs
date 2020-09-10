3b7387667778045f6ba1daf716c6a81f24ba6b87
olgamezhova <o.mezhova@dodopizza.com>
2020-09-10 23:31:23 +0300

    Add methods to generate test contact data in xml or json

--- addressbook-test-data-generators/Program.cs
+++ addressbook-test-data-generators/Program.cs
@@ -7,14 +7,65 @@ using WebAddressBookTests;
 
 namespace addressbook_test_data_generators
 {
-    static class Program
+    class Program
     {
         private static void Main(string[] args)
         {
-            var count = Convert.ToInt32(args[0]);
-            StreamWriter writer = new StreamWriter(args[1]);
-            string format = args[2];
+            string typeOfTestData = args[0];
+            var count = Convert.ToInt32(args[1]);
+            string filename = args[2];
+            string format = args[3];
+            StreamWriter writer = new StreamWriter(filename);
+            GenerateTestData(typeOfTestData, count, format, writer);
+            writer.Close();
+        }
 
+        private static void GenerateTestData(string typeOfTestData, in int count, string format,
+            StreamWriter streamWriter)
+        {
+            if (typeOfTestData.Equals("groups"))
+            {
+                GenerateGroupData(count, format, streamWriter);
+            }
+            else if (typeOfTestData.Equals("contacts"))
+            {
+                GenerateContactData(count, format, streamWriter);
+            }
+            else
+            {
+                Console.WriteLine("Unknown type of test data");
+            }
+        }
+
+        private static void GenerateContactData(in int count, string format, StreamWriter streamWriter)
+        {
+            List<ContactData> contacts = new List<ContactData>();
+
+            for (var i = 0; i < count; i++)
+            {
+                contacts.Add(new ContactData(TestBase.GenerateRandomString(10), TestBase.GenerateRandomString(10))
+                {
+                    Address = TestBase.GenerateRandomString(10),
+                    Work = TestBase.GenerateRandomPhoneNumber(10)
+                });
+            }
+
+            switch (format)
+            {
+                case "json":
+                    WriteContactsToJsonFile(contacts, streamWriter);
+                    break;
+                case "xml":
+                    WriteContactsToXmlFile(contacts, streamWriter);
+                    break;
+                default:
+                    Console.WriteLine("Unknown type of file format");
+                    break;
+            }
+        }
+
+        private static void GenerateGroupData(int count, string format, StreamWriter streamWriter)
+        {
             List<GroupData> groups = new List<GroupData>();
 
             for (var i = 0; i < count; i++)
@@ -26,27 +77,24 @@ namespace addressbook_test_data_generators
                 });
             }
 
-            if (format == "csv")
+            switch (format)
             {
-                WriteGroupsToCsvFile(groups, writer);
+                case "json":
+                    WriteGroupsToJsonFile(groups, streamWriter);
+                    break;
+                case "xml":
+                    WriteGroupsToXmlFile(groups, streamWriter);
+                    break;
+                case "csv":
+                    WriteGroupsToCsvFile(groups, streamWriter);
+                    break;
+                default:
+                    Console.WriteLine("Unknown type of file format");
+                    break;
             }
-            else if (format == "xml")
-            {
-                WriteGroupsToXmlFile(groups, writer);
-            }       
-            else if (format == "json")
-            {
-                WriteGroupsToJsonFile(groups, writer);
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized format " + format);
-            }
-
-            writer.Close();
         }
 
-        public static void WriteGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
+        private static void WriteGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
         {
             foreach (var group in groups)
             {
@@ -59,11 +107,22 @@ namespace addressbook_test_data_generators
 
         private static void WriteGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
         {
-            new XmlSerializer(typeof(List<GroupData>)).Serialize(writer,groups);
+            new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
         }
-        static void WriteGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
+
+        private static void WriteGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
         {
             writer.Write(JsonConvert.SerializeObject(groups, Formatting.Indented));
+        }
+
+        private static void WriteContactsToJsonFile(List<ContactData> contacts, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(contacts, Formatting.Indented));
+        }
+
+        private static void WriteContactsToXmlFile(List<ContactData> contacts, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<ContactData>)).Serialize(writer, contacts);
         }
     }
 }
